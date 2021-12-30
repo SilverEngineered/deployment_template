8782160f14d6743915c14157767381ce7fa3caef
Daniel Silver <daniel@silverengineered.com>
2021-12-30 12:42:47 -0500

    updated startup script for ubuntu

--- ec2_startup.sh
+++ ec2_startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt install docker
+sudo apt install docker.io
 sudo docker build -t dockerflask .
 sudo service docker start
 sudo docker run -d -p 5000:5000 dockerflask
